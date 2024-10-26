b170e5d68053ca5ec274f0b2603d7e1c0c6a2148
Maxym <homa.max007@gmail.com>
2024-10-26 09:56:16 +0300

    Added class Customer

---
+++ SpamProject/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpamProject
+{
+    public class Customer : ICustomer
+    {
+        public string Name { get; }
+
+        public Customer(string name)
+        {
+            Name = name;
+        }
+
+        public void Update(string category, string message)
+        {
+            Console.WriteLine($"For {Name}: {category} - {message}");
+        }
+    }
+}
